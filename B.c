e438ae52cc65f8c123fddb44e866c64b88a29511
Carlos Henrique <carlos_hdc@hotmail.com>
2017-06-10 12:57:00 -0300

    struct

--- B.c
+++ B.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct no{
+    int folha,Nchaves;
+    int *chaves;
+    char **filhos;
+}TNO;
+
+void mapear(char* nome){
+
+
+
+}
+
+char *buscar (){
+    return NULL;
+}
+
+
 int main(){
-
-
-
-
+    char *v = buscar();
+    if (v){
+        printf("%s\n",v);
+    }
+    return 0;
 }
